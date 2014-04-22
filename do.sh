c9b22431b9f981916b242a845b6610bc601dbb37
marcio <taboca@gmail.com>
2014-04-22 14:59:33 -0300

    Initial files

--- do.sh
+++ do.sh
@@ -3,8 +3,6 @@ cordova create app --name CompanionApp --id com.telasocial.companion
 
 #cordova platform add android
 
-git clone https://github.com/TelaSocial/companion-tests.git
-
 cd app
 
 cordova plugin add https://github.com/EddyVerbruggen/Calendar-PhoneGap-Plugin.git
@@ -17,17 +15,9 @@ cordova platform add android
 
 #cp ./plugins/nl.x-services.plugins.calendar/www/Calendar.js ../www/Calendar.js
 
-cp config.xml config2.xml 
-
-sed 's/index/parse/g' config2.xml > config.xml
-
-cp -r ../companion-tests/parse-collect/* ./www
+cp -r ../www/* ./www
 
 cd www
-
-cp ./parse.html ./parse2.html
-
-sed 's/<include>/<script\ type=\"text\/javascript\"\ src=\"cordova\.js\"><\/script><script\ type=\"text\/javascript\"\ src=\"js\/index.js\"><\/script><script\ type=\"text\/javascript\">app\.initialize();<\/script>/g' parse2.html > parse.html 
 
 cordova run
 
---
+++ www/index.js
@@ -0,0 +1,21 @@
+    $(document).ready(function () { 
+
+
+    }); 
+
+
+    function callCalendar() { 
+
+        var startDate = new Date(2014,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
+        var endDate = new Date(2014,2,15,19,30,0,0,0);
+        var title = "My nice event";
+        var location = "Home";
+        var notes = "Some notes about this event.";
+        var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+        var error = function(message) { alert("Error: " + message); };
+
+        window.plugins.calendar.createEventInteractively(title,location,notes,startDate,endDate,success,error);
+
+    }
+
+
